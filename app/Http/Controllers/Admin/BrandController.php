60d619c51c4623cb16ffffc0f0ff47e2eafcb235
Mithun Halder <mithunrptc@gmail.com>
2019-09-16 21:30:04 +0600

    Brand CRUD added

--- app/Http/Controllers/Admin/BrandController.php
+++ app/Http/Controllers/Admin/BrandController.php
@@ -2,10 +2,123 @@
 
 namespace App\Http\Controllers\Admin;
 
+use App\Contracts\BrandContract;
+use App\Http\Controllers\BaseController;
 use Illuminate\Http\Request;
-use App\Http\Controllers\Controller;
 
-class BrandController extends Controller
+class BrandController extends BaseController
 {
-    //
+    /**
+     * @var BarndContract
+     */
+
+    protected $brandRepository;
+
+
+    /**
+     * BrandController construct
+     * @param BrandContract $brandRepository
+     */
+
+     public function __construct(BrandContract $brandRepository)
+     {
+        $this->brandRepository = $brandRepository;
+     }
+
+
+     /**
+      * @return \Illuminate\Contracts\View\Factory\Illuminate\View\View
+      */
+
+      public function index(){
+          $brands = $this->brandRepository->listBrands();
+
+          $this->setPageTitle('Brands','List of all brands');
+
+          return view('admin.brands.index', compact('brands'));
+      }
+
+
+      /**
+       * @return \Illuminate\Contracts\View\Factory\Illuminate\View\View
+       */
+
+       public function create(){
+           $this->setPageTitle('Brands','Create Brand');
+           return view('admin.brands.create');
+       }
+
+
+    /**
+     * @param Request $request
+     * @return \Illuminate\Http\RedirectResponse
+     * @throws  \Illuminate\Validation\ValidationException
+     */
+
+       public function store(Request $request){
+            $this->validate($request,[
+                'name'          =>  'required|max:191',
+                'logo'          =>  'mimes:png,jpg,jpeg|max:1000'
+            ]);
+
+            $params = $request->except('_token');
+
+            $brand = $this->brandRepository->createBrand($params);
+
+            if(!$brand){
+                return $this->responseRedirectBack('Error occurred while createing brand.', 'error', true, true);
+            }
+
+            return $this->responseRedirect('admin.brands.index', 'Brand added successfully', 'success', false, false);
+       }
+
+    /**
+     * @param int $id
+     * @return \Illuminate\Contracts\View\Factory\Illuminate\View\View
+     */
+
+     public function edit($id){
+         $brand = $this->brandRepository->findBrandById($id);
+         $this->setPageTitle('Brand', 'Edit brand : '.$brand->name);
+         return view('admin.brands.edit', compact('brand'));
+     }
+
+
+     /**
+      * @param Request $request
+      * @return \Illuminate\Http\RedriectResponse
+      * @throws \Illuminate\Validation\ValidationException
+      */
+
+     public function update(Request $request){
+        $this->validate($request,[
+            'name'          =>  'required|max:191',
+            'logo'          =>  'mimes:png,jpg,jpeg|max:1000'
+        ]);
+
+        $params = $request->except('_token');
+
+        $brand = $this->brandRepository->updateBrand($params);
+
+        if(!$brand){
+            return $this->responseRedirectBack('Error occurred while updating brand.', 'error', true, true);
+        }
+
+        return $this->responseRedirectBack('Brand updated successfully', 'success', false, false);
+     }
+
+
+     /**
+      * @param int $id
+      * @return \Illuminate\Http\RedirectResponse
+      */
+
+      public function destroy($id){
+        $brand = $this->brandRepository->deleteBrand($id);
+
+        if(!$brand){
+            return $this->responseRedirectBack('Error occurred while deleting brand', 'error', true, true);
+        }
+        return $this->responseRedirect('admin.brands.index','Brand deleted successfully', 'success', false, false);
+      }
 }
---
+++ resources/views/admin/brands/edit.blade.php
@@ -0,0 +1,58 @@
+@extends('admin.app')
+
+@section('title')
+{{ $pageTitle }}
+@endsection
+
+@section('content')
+    <div class="app-title">
+        <div>
+            <h1><i class="fa fa-briefcase"></i></h1>
+        </div>
+    </div>
+    @include('admin.partials.flash')
+
+    <div class="row">
+        <div class="col-md-8 mx-auto">
+            <div class="tile">
+                <h3 class="tile-title">{{ $subtitle }}</h3>
+                <form action="{{ route('admin.brands.store') }}" method="POST" enctype="multipart/form-data">
+                    @csrf
+
+                    <div class="tile-body">
+                        <div class="form-group">
+                            <label for="name" class="control-label">Name <span class="m-l-5 text-danger">*</span></label>
+                            <input type="text" name="name" id="name" value="{{ old('name', $brand->name) }}" class="form-control @error('name') is-invalid @enderror">
+                            @error('name')
+                            <span class="invalid-feedback">{{ $message }}</span>
+                            @enderror
+                        </div>
+                        <div class="form-group">
+                            <div class="row">
+                                @if($brand->logo != null)
+                                <div class="col-md-2">
+                                    <figure class="mt-2" style="width: 80px; height: auto">
+                                        <img src="{{ asset('storage/'.$brand->logo) }}" alt="" id="brandLogo" class="img-fluid">
+                                    </figure>
+                                </div>
+
+                                @enderror
+                                <div class="col-md-10">
+                                    <label for="logo" class="control-label">Brand Logo</label>
+                                    <input class="form-control @error('logo') is-invalid @enderror" type="file" name="logo" id="logo" >
+                                    @error('logo')
+                                    <span class="invalid-feedback">{{ $message }}</span>
+                                    @enderror
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="tile-footer">
+                        <button class="btn btn-primary" type="submit"> <i class="fa fa-fw fa-lg fa-check-circle"></i> Save Brand</button>
+                        <a href="{{ route('admin.brands.index') }}" class="btn btn-secondary"> <i class="fa fa-fw lg fa-times-circle"></i> Cancel</a>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+@endsection
